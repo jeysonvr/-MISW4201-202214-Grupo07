055520a71c112bc8da502ae1e15cb0d71f51332d
VivianaReyV <z.rey@uniandes.edu.co>
2022-09-11 14:03:27 -0500

    Update modelos.py

--- microservicio_1/modelos/modelos.py
+++ microservicio_1/modelos/modelos.py
@@ -4,7 +4,7 @@ from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
 db = SQLAlchemy()
 
-// Información de los mensajes enviados
+# Información de los mensajes enviados
 class LogEnviados(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date_send = db.Column(db.String(128))
